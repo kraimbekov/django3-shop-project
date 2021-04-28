3862b30ccf2ec69643d40c5f8ba53988cc60d456
Raimbekov Kuanish <ikana.krut@gmail.com>
2021-04-28 23:21:41 +0600

    add static

--- shop_project/models.py
+++ shop_project/models.py
@@ -1,12 +1,16 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     img = models.ImageField(upload_to='blog_img/',blank=True)
     text = models.TextField()
+    create_date = models.DateField(default=datetime.datetime.now,blank=True)
 
     def __str__(self):
         return self.title
